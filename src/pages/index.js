2e6fa66bf4437b285d6220d0d32f96bd09ede9d2
Ajdin <aydinholjan@gmail.com>
2019-06-02 00:54:39 +0200

    ad

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = props => {
       <Head title="Home"/>
       <h1>Hello </h1>
       <h2>I am Ajdin.Front - end developer.Living in Sarajevo.</h2>
-      <p>
-        Need a developer ? <Link to="/contact">Contact me</Link>
-      </p>
     </Layout>
   )
 }
